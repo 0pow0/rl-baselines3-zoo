fa89387280e526a1d7c02744c9a6cfbf1a22a61f
rui <>
2024-05-02 11:35:57 -0400

    sync

--- rl_zoo3/enjoy.py
+++ rl_zoo3/enjoy.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, '/home/rzuo02/work/rl-baselines3-zoo')
+sys.path.insert(0, '/home/rzuo02/work/explain-mario-ppo/NN-Explainer/src')
 import argparse
 import importlib
 import os
@@ -17,11 +21,10 @@ from rl_zoo3.exp_manager import ExperimentManager
 from rl_zoo3.load_from_hub import download_from_hub
 from rl_zoo3.utils import StoreDict, get_model_path
 import rui.rui_id as rui
+from stable_baselines3.common.policies import ActorCriticPolicy
+import pytorch_lightning as pl
 
-import os
-import sys
-sys.path.insert(0, '/home/rzuo02/work/rl-baselines3-zoo')
-sys.path.insert(0, '/home/rzuo02/work/explain-mario-ppo/NN-Explainer/src')
+
 
 from data.dataset import AtariDataset
 
@@ -33,7 +36,159 @@ import torch
 from PIL import Image
 import cv2
 
-dataset = AtariDataset("/home/rzuo02/work/rl-baselines3-zoo/datasets/Enduro/")
+def get_rl_baselines3_model_path(algo, env_name, folder_trained_agents):
+    # Experiment ID (default: 0: latest, -1: no exp folder)"
+    exp_id = 0
+    folder = folder_trained_agents
+    try:
+        _, model_path, log_path = get_model_path(
+            exp_id,
+            folder,
+            algo,
+            env_name,
+        )
+    except (AssertionError, ValueError) as e:
+        # Special case for rl-trained agents
+        # auto-download from the hub
+        if "rl-trained-agents" not in folder:
+            raise e
+        else:
+            print("Pretrained model not found, trying to download it from sb3 Huggingface hub: https://huggingface.co/sb3")
+            # Auto-download
+            download_from_hub(
+                algo=algo,
+                env_name=env_name,
+                exp_id=exp_id,
+                folder=folder,
+                organization="sb3",
+                repo_name=None,
+                force=False,
+            )
+            # Try again
+            _, model_path, log_path = get_model_path(
+                exp_id,
+                folder,
+                algo,
+                env_name,
+            )
+
+    print(f"Loading {model_path}")
+    return model_path, log_path
+
+def get_rl_baselines3_model(algo:str, environment_name:str, seed:int, folder_trained_agents:str, device):
+
+    # Options when loading rl_zoo3 models
+    env_name: EnvironmentName = EnvironmentName(environment_name)
+
+    # adopted from rl_zoo3
+    model_path, log_path = get_rl_baselines3_model_path(
+        algo=algo,
+        env_name=env_name,
+        folder_trained_agents=folder_trained_agents
+    )
+
+    # adopted from rl_zoo3
+    newer_python_version = sys.version_info.major == 3 and sys.version_info.minor >= 8
+    custom_objects = {}
+    if newer_python_version:
+        custom_objects = {
+            "learning_rate": 0.0,
+            "lr_schedule": lambda _: 0.0,
+            "clip_range": lambda _: 0.0,
+        }
+        
+    # adopted from rl_zoo3
+    stats_path = os.path.join(log_path, env_name)
+    hyperparams, maybe_stats_path = get_saved_hyperparams(
+        stats_path,
+        norm_reward=False,
+        test_mode=True
+    )
+
+    # adopted from rl_zoo3
+    # Off-policy algorithm only support one env for now
+    off_policy_algos = ["qrdqn", "dqn", "ddpg", "sac", "her", "td3", "tqc"]
+    kwargs = dict(seed=seed)
+    if algo in off_policy_algos:
+        # Dummy buffer size as we don't need memory to enjoy the trained agent
+        kwargs.update(dict(buffer_size=1))
+        # Hack due to breaking change in v1.6
+        # handle_timeout_termination cannot be at the same time
+        # with optimize_memory_usage
+        if "optimize_memory_usage" in hyperparams:
+            kwargs.update(optimize_memory_usage=False)
+        
+    # load env_kwargs if existing
+    env_kwargs = {}
+    args_path = os.path.join(log_path, env_name, "args.yml")
+    if os.path.isfile(args_path):
+        with open(args_path) as f:
+            loaded_args = yaml.load(f, Loader=yaml.UnsafeLoader)
+            if loaded_args["env_kwargs"] is not None:
+                env_kwargs = loaded_args["env_kwargs"]
+    
+    env = create_test_env(
+        env_name.gym_id,
+        n_envs=1,
+        stats_path=maybe_stats_path,
+        seed=seed,
+        log_dir=None,
+        should_render=False,
+        hyperparams=hyperparams,
+        env_kwargs=env_kwargs,
+    )
+
+    if "HerReplayBuffer" in hyperparams.get("replay_buffer_class", ""):
+        kwargs["env"] = env
+
+    model = ALGOS['ppo'].load(
+        model_path,
+        custom_objects=custom_objects,
+        device=device,
+        **kwargs
+    )
+    return model
+class AtariPPOModel(pl.LightningModule):
+    def __init__(self, *args: torch.Any, **kwargs: torch.Any) -> None:
+        super().__init__()
+        self.setup_model(**kwargs)
+
+    def setup_model(self, env_name, folder_trained_agents, fix_model=True):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # Options when loading rl_zoo3 models
+        algo = 'ppo'
+        seed = 0
+        
+        self.model = get_rl_baselines3_model(
+            algo=algo,
+            environment_name=env_name,
+            seed=seed,
+            folder_trained_agents=folder_trained_agents,
+            device=self.device
+        )
+        self.model.policy.to(device)
+
+        if fix_model:
+            self.model.policy.set_training_mode(False)
+            self.model.policy.eval()
+            for param in self.model.policy.parameters():
+                param.requires_grad = False
+
+    # (batch, 4, 84, 84)
+    def forward(self, x):
+        # preprocessed_obs = preprocess_obs(x, self.model.policy.observation_space, normalize_images=self.model.policy.normalize_images)
+        # features = self.model.policy.pi_features_extractor(preprocess_obs)
+
+        # print(type(self.model.policy), type(self.model.policy.extract_features))
+        # print(super(type(self.model.policy), self.model.policy), super(type(self.model.policy), self.model.policy).extract_features)
+        # print(super(ActorCriticPolicy, self.model.policy), super(ActorCriticPolicy, self.model.policy).extract_features)
+        # print(super(BaseModel, self.model.policy), super(BaseModel, self.model.policy).extract_features)
+        features = super(ActorCriticPolicy, self.model.policy).extract_features(x, self.model.policy.pi_features_extractor)
+        latent_pi = self.model.policy.mlp_extractor.forward_actor(features)
+        logits = self.model.policy.action_net(latent_pi)
+        # return self.model.policy.action_dist.proba_distribution(action_logits=logits).get_actions(deterministic=True)
+        return logits
+
 # change coefficient of default torch grayscale for R: 0.2989 -> 0.299 to achieve consistency with cv2
 def rgb_to_grayscale(img, num_output_channels: int = 1):
     if img.ndim < 3:
@@ -251,6 +406,10 @@ def enjoy() -> None:  # noqa: C901
     model = ALGOS[algo].load(model_path, custom_objects=custom_objects, device=args.device, **kwargs)
     obs = env.reset()
     print(obs.shape)
+    ppo = AtariPPOModel(
+        env_name= 'Seaquest' + 'NoFrameskip-v4',
+        folder_trained_agents='/home/rzuo02/work/rl-baselines3-zoo/rl-trained-agents'
+    )
 
     # Deterministic by default except for atari games
     stochastic = args.stochastic or (is_atari or is_minigrid) and not args.deterministic
@@ -325,6 +484,7 @@ def enjoy() -> None:  # noqa: C901
                 episode_start=episode_start,
                 deterministic=True,
             )
+            print("obs.shape", obs.shape, "obs.dtype", obs.dtype)
             # action_new, __ = model.predict(
             #     x,  # type: ignore[arg-type]
             #     state=lstm_states,
@@ -345,10 +505,24 @@ def enjoy() -> None:  # noqa: C901
             # print(action, anno)
             # exit()
             # print('rui.already_reset ', rui.already_reset)
-            if (rui.empty == 0) and (rui.already_reset == 1):
+            # if (rui.empty == 0) and (rui.already_reset == 1):
+            if (rui.empty == 0):
                 for i in range(4):
                     # (1, 210, 160, 12)
-                    x = rui.stacked_frames
+                    # x = torch.from_numpy(rui.stacked_frames)
+                    # print("x.shape", x.shape)
+                    # x = torch.split(x, 3, dim=3)
+                    # x = torch.cat(x, dim=2).permute(0, 3, 1, 2)
+                    # transformer = T.Compose([T.Lambda(rgb_to_grayscale), T.Lambda(squeeze_rgb_gray_channel), T.Resize(size=(84, 4*84))])
+                    # x = transformer(x)
+                    # x = torch.stack(x.split(84, 2), 1)
+                    # print(x.shape)
+                    # print(x[0].shape, x[1].shape, x[2].shape, x[3].shape)
+                    # print(x.shape)
+                    # assert(np.all(x[0] == rui.stacked_frames[0, :, :, 0:3]))
+                    # assert(np.all(x[1] == rui.stacked_frames[0, :, :, 3:6]))
+                    # assert(np.all(x[2] == rui.stacked_frames[0, :, :, 6:9]))
+                    # assert(np.all(x[3] == rui.stacked_frames[0, :, :, 9:]))
                     # # print(x.shape)
                     # save_img(x[0, :, :, 0:3], 'x_full0.png')
                     # save_img(x[0, :, :, 3:6], 'x_full1.png')
@@ -386,16 +560,16 @@ def enjoy() -> None:  # noqa: C901
                     # assert(action == action_new)
                     # break
 
-                    xs = []
-                    for j in range(4):
-                        xs.append(foo(x[0, :, :, 3*j:3*j+3]))
-                    x = np.stack(xs, axis=2)[np.newaxis, ...]
-                    print(x.shape, obs.shape)
+                    # xs = []
+                    # for j in range(4):
+                    #     xs.append(foo(x[0, :, :, 3*j:3*j+3]))
+                    # x = np.stack(xs, axis=2)[np.newaxis, ...]
+                    # print(x.shape, obs.shape)
                     # print(x)
                     # print(torch.from_numpy(x).shape)
                     # print(torch.from_numpy(x))
                     # exit()
-                    print((x == obs).all())
+                    # print((x == obs).all())
                     # save_img(x[0, :, :, 0], 'x0.png')
                     # save_img(x[0, :, :, 1], 'x1.png')
                     # save_img(x[0, :, :, 2], 'x2.png')
@@ -404,19 +578,69 @@ def enjoy() -> None:  # noqa: C901
                     # save_img(obs[0, :, :, 1], 'obs1.png')
                     # save_img(obs[0, :, :, 2], 'obs2.png')
                     # save_img(obs[0, :, :, 3], 'obs3.png')
-                    action_new, lstm_states = model.predict(
-                        x,  # type: ignore[arg-type]
-                        state=lstm_states,
-                        episode_start=episode_start,
-                        deterministic=True,
-                    )
-                    print(action, action_new, action_new == action)
-                    assert(action_new == action)
-                    # save_img(rui.stacked_frames[0, :, :, 3*i : 3*i +3], f'{args.dataset_path}/{args.atari_env}/{rui.frame_id}_{i}.png')
+                    # action_new, lstm_states = model.predict(
+                    #     x,  # type: ignore[arg-type]
+                    #     state=lstm_states,
+                    #     episode_start=episode_start,
+                    #     deterministic=True,
+                    # )
+                    # print(action, action_new, action_new == action)
+                    # assert(action_new == action)
+                    save_img(rui.stacked_frames[0, :, :, 3*i : 3*i +3], f'{args.dataset_path}/{args.atari_env}/{rui.frame_id}_{i}.png')
                     # save_img(obs[0, :, :, i], f'foo_{i}.png')
                     # save_img(obs[0, :, :, i], f'{args.dataset_path}/{args.atari_env}/{rui.frame_id}_{i}.png')
                     pass
-                # df.loc[len(df)] = [rui.frame_id, action[0]]
+
+                x = torch.from_numpy(rui.stacked_frames)
+                # print(x)
+                print("x.shape", x.shape)
+                print(x.dtype)
+                x = torch.split(x, 3, dim=3)
+                x = torch.cat(x, dim=2).permute(0, 3, 1, 2)
+                transformer = T.Compose([T.Lambda(rgb_to_grayscale), T.Lambda(squeeze_rgb_gray_channel), T.Resize(size=(84, 4*84))])
+                print("x.shape", x.shape)
+                print(x.dtype)
+                x = transformer(x)
+                x = torch.stack(x.split(84, 2), 1)
+                print(x.shape)
+                print("lstm_states", lstm_states, "episode_start", episode_start)
+                __action, _ = model.predict(
+                    x.permute(0, 2, 3, 1).numpy(),  # type: ignore[arg-type]
+                    state=lstm_states,
+                    episode_start=episode_start,
+                    deterministic=True,
+                )
+
+                imgs = []
+                for z in range(4):
+                    img = Image.open( f'{args.dataset_path}/{args.atari_env}/{rui.frame_id}_{z}.png').convert("RGB")
+                    imgs.append(img.copy())
+                imgs = [T.PILToTensor()(img) for img in imgs] 
+                imgs = torch.cat(imgs, dim=2)
+                print(imgs.shape)
+                x2 = transformer(imgs.unsqueeze(0))
+                x2 = torch.stack(x2.split(84, 2), 1)
+                __action2, _ = model.predict(
+                    x2.permute(0, 2, 3, 1).numpy(),  # type: ignore[arg-type]
+                    state=lstm_states,
+                    episode_start=episode_start,
+                    deterministic=True,
+                )
+                print(__action2, __action)
+                assert(__action2 == __action)
+                # exit()
+
+
+                # features = super(ActorCriticPolicy, model.policy).extract_features(x, model.policy.pi_features_extractor)
+                # latent_pi = model.policy.mlp_extractor.forward_actor(features)
+                # logits = model.policy.action_net(latent_pi)
+                logits = ppo(x)
+                assert(logits.argmax().item() == __action)
+                # print("lstm_states", lstm_states, "episode_start", episode_start)
+                # x = x.permute(0, 2, 3, 1).numpy()
+                # print("x.shape", x.shape, "x.dtype", x.dtype)
+                # exit()
+                df.loc[len(df)] = [rui.frame_id, __action[0]]
                 rui.empty = 1
                 # img = Image.open('/home/rzuo02/work/rl-baselines3-zoo/datasets/Enduro/9999_0.png').convert("L") 
                 # print(T.PILToTensor()(img))
@@ -510,7 +734,7 @@ def enjoy() -> None:  # noqa: C901
 
     env.close()
     df = df.sort_index()
-    # df.to_csv(f'{args.dataset_path}/{args.atari_env}/annotations.csv', index=False)
+    df.to_csv(f'{args.dataset_path}/{args.atari_env}/annotations.csv', index=False)
 
 
 if __name__ == "__main__":
